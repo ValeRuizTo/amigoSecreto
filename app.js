5afe7ee92d703e3bc973edb2b3bc39acf9fca49b
ValeRuizTo <valentinaruito@unisabana.edu.co>
2025-08-31 18:47:10 -0500

    commit1

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres= [];
+
+function agregarAmigo() {
+    let nombre= document.getElementById("amigo").value;
+    if (nombre.trim() === "") {
+          alert("Por favor, ingresa un nombre válido.");
+            return;
+        }
+        nombres.push(nombre);
+        mostrarAmigos();
+        document.getElementById("amigo").value = "";
+
+    }
+
+    function mostrarAmigos() {
+        let lista = document.getElementById("listaAmigos");
+        lista.innerHTML = "";
+        nombres.forEach(function(nombre) {  
+            let li = document.createElement("li");
+            li.textContent = nombre;
+            lista.appendChild(li);
+        });
+    }
+
+function sortearAmigo(){
+    let indiceAleatorio = Math.floor(Math.random() * nombres.length);
+    let amigoSorteado = nombres[indiceAleatorio];
+    let lista = document.getElementById("listaAmigos");
+        lista.innerHTML = "";
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "El amigo secreto es: " + amigoSorteado;
+}
+
